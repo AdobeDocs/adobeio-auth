b361df00fc15e6477bd6294e9a608b32c4e7c439
Hiren Anand Shah <hirenanandshah@outlook.com>
2019-04-19 09:24:35 -0600

    Using @adobe/jwt-auth

--- JWT/samples/adobe-jwt-node/config.js
+++ JWT/samples/adobe-jwt-node/config.js
@@ -1,13 +1,38 @@
+//console.adobe.io -> Integration -> Details
+
 module.exports = {
-  credentials: {
-    api_key: "670b24b901af4a35a8dff464b.....",
-    technical_account_id: "522A38415BDC804D.....C@techacct.adobe.com",
-    client_secret: "dc5d1e7e-9216-46e2-b8d0-ae43.......",
-    org_id: "8F99160E571FC0427F.....@AdobeOrg",
-    private_key:
-      "-----BEGIN PRIVATE KEY-----\n" +
-      "YOUR PRIVATE KEY\n" +
-      "-----END PRIVATE KEY-----",
-    meta_scopes: "https://ims-na1.adobelogin.com/s/<META_SCOPE>"
-  }
-};
+    credentials: {
+        clientId : "bb11ebd6406a4f---------f705b26a", //Application ID (Client ID)
+        technicalAccountId : "8EDD3561-------5CDB@techacct.adobe.com", //Technical account ID
+        orgId : "EF2D363--------CA@AdobeOrg", // Org ID
+        clientSecret : "c811f67e-9ed------------25ea148", //Client Secret
+        metaScopes : "ent_user_sdk", // Metascopes
+        privateKey : "-----BEGIN RSA PRIVATE KEY-----\n" + //Content of private.key
+        "MIIEowIBAAKCAQEAtqX3bHwETnyOizuDUWoyT0PTreyQJ9Hkti+5CssnRULAsE/D\n" +
+        "Yw6RyB8WLlC07mYxUkA/7Ikd1oEvTuAYLCSuJWXu0WBhXHrw6268xP60LjQl1Xu5\n" +
+        "GQjrTiQll8c0N10dww6TzzupLyFOD/J+TsH9T2zZ/DYukP/x0iOGs/7D3IvJfRcq\n" +
+        "6FOoAcKSV3geW7IRDnmn+dX4W3SWN/SS9OGRgYu16KBYV2+YvAkRtWM3nMdLuz6a\n" +
+        "ep16MBZJQErYguowmhrSmLRFOzkZbXPjr6JZmZaNuuPLQnLYLA2p0RUzva5PThkw\n" +
+        "esidMH2WZQ5dP+4aDA8mwz6eiI6z4+WR8gp3DwIDAQABAoIBADmMN1YGCTFVi3AR\n" +
+        "E0IOrMVOwJ5XH+B8AIz------------------------------ZzVw45acX1JfMdk\n" +
+        "UhlugTQlVsa/kE2cKtZzMKT8/3ZjVdxavCpIJiObvgCBclFTVHJF4Ip16MuBud/K\n" +
+        "bsOP2akxycjupWNoUR/9s4544etV0smbm2wV/KUVth8VFdRMaE2b0xHeCOvS4MP2\n" +
+        "j1sSqdSmKUBddooFjQwDC/poOQC0GYj+VXEiHrR/uk/P/ipO2Q7N4+2DIDXAe1d7\n" +
+        "0A6reBaPagbvH---------------------------oX52CDVFJRQzncCXe6WD9tPT\n" +
+        "ldx9F6ECgYEA8fMntK4pjLKVGkmjuHCmYZqCQfElbxZWBGgeTi6TtQAC/K0NqivL\n" +
+        "57SMjiZEdE1Ct716PHs8YCHNVQVdPD2Bpv9HXq9Jcz+nQj0onofqsplnj/0nH3ls\n" +
+        "fJGo27nB8LYDINWXEY3Mr8H7/Xs5/lQIDdoadWriJHCifO0sSdih/b8CgYEAwUE6\n" +
+        "/PtHPp315mI33azUwZDZ+G4DwI2WICoGs9Z1RlqH8Zk3X11djpGurNNK5/JcPtSS\n" +
+        "No4CAEX4VYue+N2jtQnJNzH+IhLFBGDwz1JMBMBw1ye8SVtvvgYyeRnmdmsGP1cj\n" +
+        "Lb8jQckg/1q6lnJvHLq39P8uhAYmNPU7HTojerECgYA1K1w0ZnmXA7+kcT5bT9Yg\n" +
+        "aNXrK9UwHlZ0zXSrdazb/86oPFdeaBV9K74fluVAMaJRwn/UPri733HCF1Wv4UxW\n" +
+        "qI7Ejfy0KO7WrZ35iKDY8sFBcT5huktO7KuQ7bdi8HyrgIScRO8QSK7Zu3S5ITpL\n" +
+        "4PwjhYOAwofSsEQlTkf6/QKBgAR9spQ8dqxi2VsQP0IciRkyd45JaDbqU2nN8Dfv\n" +
+        "rOwEoJIhwGKr9cehZRDJHD2FqVUgdYFlDXDaL2o1g20/IVXMs2tf8wrxqrhuVEpN\n" +
+        "HE0j97tYRgziXhdpZ3TfADcSR6PjL4MZpQUbWnP0tM9YO3LMeAfugjM5PVwPst78\n" +
+        "AeURAoGBANQ0s6p/ZGV9pmteoN452Oz+PHD/WCc1wid9m2eKawqNYRODC6ls1eQ+\n" +
+        "h1veQozYJLLob3+fVgKiHo/KEtA/4Svq7/+2eW4rBs12C6N+bSBj01no6nmqrDrO\n" +
+        "j+fOdGjzGxHSiqI8QvKa+9JJodXzfKNT2ALzV/Q7h6OHjsP549Ze\n" +
+        "-----END RSA PRIVATE KEY-----\n"
+    }
+}
